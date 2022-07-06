525cd0dc8b566c4719d424964751b01aa3a23634
kendallli587 <kendallli587@gmail.com>
2022-07-06 10:34:45 -0600

    update

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 // Array Visualizer
 
+// HTML Variables
+containerEl = document.getElementById("container")
+
 // global Variable
-let myData = [400, 500, 250, 550, 100, 300];
+let myData = [200, 500, 250, 550, 100, 300, 600];
 
 // Display Data
-function drawArray() {
-    for (let i = 0; i < myData.length; i++) {
-        console.log(myData[i]);
-    }
-}
-
 drawArray();
+
+function drawArray() {
+    let outputStr = "";
+    for (let i = 0; i < myData.length; i++) {
+        outputStr += `<div>${myData[i]}</div>`
+    }
+    containerEl.innerHTML = outputStr;
+}
