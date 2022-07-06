f3f9426b77e18ed23b9973ba2af7c9657a410389
kendallli587 <kendallli587@gmail.com>
2022-07-06 10:31:00 -0600

    update

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+// Array Visualizer
+
+// global Variable
+let myData = [400, 500, 250, 550, 100, 300];
+
+// Display Data
+function drawArray() {
+    for (let i = 0; i < myData.length; i++) {
+        console.log(myData[i]);
+    }
+}
+
+drawArray();
