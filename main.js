16a2108cbce3ba6a37c774b61c6da1a4ff24d07c
kendallli587 <kendallli587@gmail.com>
2022-07-06 10:42:09 -0600

    update

--- main.js
+++ main.js
@@ -4,7 +4,10 @@
 containerEl = document.getElementById("container")
 
 // global Variable
-let myData = [200, 500, 250, 550, 100, 300, 600];
+let myData = [];
+for (let n = 1; n <= 100; n++) {
+    myData.push(Math.random() * 600);
+}
 
 // Display Data
 drawArray();
@@ -12,7 +15,7 @@ drawArray();
 function drawArray() {
     let outputStr = "";
     for (let i = 0; i < myData.length; i++) {
-        outputStr += `<div>${myData[i]}</div>`
+        outputStr += `<div style="height: ${myData[i]}px"></div>`
     }
     containerEl.innerHTML = outputStr;
 }
